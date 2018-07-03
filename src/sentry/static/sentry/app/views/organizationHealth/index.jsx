877df1d3d058596c716c5bb4a39f378e74298255
Billy Vong <billy@sentry.io>
2018-07-03 13:48:01 -0700

    add health header

--- src/sentry/static/sentry/app/views/organizationHealth/index.jsx
+++ src/sentry/static/sentry/app/views/organizationHealth/index.jsx
@@ -1,8 +1,14 @@
+import {Flex, Box} from 'grid-emotion';
+import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'react-emotion';
 
 import {t} from 'app/locale';
 import AsyncView from 'app/views/asyncView';
 import Feature from 'app/components/feature';
+import HeaderSeparator from 'app/components/organizations/headerSeparator';
+import MultipleProjectSelector from 'app/components/organizations/multipleProjectSelector';
+import TimeRangeSelector from 'app/components/organizations/timeRangeSelector';
 
 class OrganizationHealth extends AsyncView {
   getTitle() {
@@ -16,9 +22,18 @@ class OrganizationHealth extends AsyncView {
   renderBody() {
     return (
       <Feature feature={['health']} showNoFeatureMessage>
+        <Header>
+          <MultipleProjectSelector />
+          <HeaderSeparator />
+          <TimeRangeSelector />
+        </Header>
         <div>hello2</div>
       </Feature>
     );
   }
 }
 export default OrganizationHealth;
+
+const Header = styled(Flex)`
+  font-size: 18px;
+`;
